9b1ca27a091b6f68379206997b8c79c84dcf1034
Vanessaines2 <Vanessaines2@icloud.com>
2023-05-11 12:30:31 -0400

    async function and starting to test database

--- db/index.js
+++ db/index.js
@@ -3,3 +3,16 @@ const client = new Client("postgres://localhost:5432/juicebox-dev");
 module.exports = {
   client,
 };
+
+async function getAllUsers() {
+  const { rows } = await client.query(
+    `SELECT id, username
+        FROM users;
+        `
+  );
+  return rows;
+}
+module.exports = {
+  client,
+  getAllUsers,
+};
--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,51 @@
-const { client } = require("./index");
+const { client, getAllUsers } = require("./index");
+
+async function dropTables() {
+  try {
+    console.log("starting to drop tables...");
+    await client.query(` DROP TABLE IF EXISTS users;`);
+    console.log(" finished dropping tables!");
+  } catch (error) {
+    console.error("Error dropping tables");
+    throw error;
+  }
+}
+
+async function createTables() {
+  try {
+    console.log("starting to build tables...");
+    await client.query(`
+    CREATE TABLE  users (
+        id SERIAL PRIMARY KEY, 
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar (255) NOT NULL
+    );
+    `);
+    console.log("finished building tables!");
+  } catch (error) {
+    console.error("error building tables!");
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+}
 
 async function testDB() {
   try {
-    client.connect();
-    const { rows } = await client.query(`SELECT * FROM users;`);
-    console.log(rows);
+    console.log("starting to test database...");
+    const users = await getAllUsers();
+    // client.connect();
+    // const { users } = await client.query(`SELECT * FROM users;`);
+    console.log(users);
   } catch (error) {
     console.error(error);
   } finally {
