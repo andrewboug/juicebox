666f7423d4fb5b2b641184d291aa37f5e156bcdc
Vanessaines2 <Vanessaines2@icloud.com>
2023-05-18 11:46:01 -0400

    created users and posts routes

---
+++ api/users.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const usersRouter = express.Router();
+
+usersRouter.use((req, res, next) => {
+  console.log("A request is being made to /users");
+  next();
+  //   res.send({ message: "hello from /users! from Andrew & vanessa !" });
+});
+
+const { getAllUsers } = require("../db");
+
+usersRouter.get("/", async (req, res) => {
+  const users = await getAllUsers();
+
+  res.send({
+    users,
+  });
+});
+
+module.exports = usersRouter;
