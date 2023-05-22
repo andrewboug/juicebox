79bd1fd24408e7f47f6d8042a8410d93fad69178
Vanessaines2 <Vanessaines2@icloud.com>
2023-05-21 22:26:35 -0400

    finishedd

--- api/tags.js
+++ api/tags.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const tagsRouter = express.Router();
+const { getPostsByTagName, getAllTags } = require("../db");
 
 tagsRouter.use((req, res, next) => {
   console.log("a  request is being made to /tags");
   next();
 });
-
-const { getAllTags } = require("../db");
 
 tagsRouter.get("/", async (req, res) => {
   const tags = await getAllTags();
@@ -14,6 +13,25 @@ tagsRouter.get("/", async (req, res) => {
   res.send({
     tags,
   });
+});
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  const { tagName } = req.params;
+
+  try {
+    const posts = await getPostsByTagName(tagName);
+
+    if (posts) {
+      res.send({ posts: posts });
+    } else {
+      next({
+        name: "Error",
+        message: "No posts matching that tagName",
+      });
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
